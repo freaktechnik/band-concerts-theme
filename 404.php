e3bc5ed06a78ec870a9a8f148967e247564af1fb
Martin Giger <martin@humanoids.be>
2017-08-09 20:37:21 +0200

    add error page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<div>
+    <article id="error-404" class="two-columns error">
+        <aside>
+            <h1>Seite nicht gefunden</h1>
+        </aside>
+        <section>
+            <p>Der gewünschte Inhalt ist nicht verfügbar. Hoffentlich erreichen Sie Ihr Ziel über die Navigation.</p>
+        </section>
+    </article>
+<?php get_footer(); ?>
