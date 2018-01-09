419a9eda399b4541e3e3e8b5ce1c53c654c596ad
Martin Giger <martin@humanoids.be>
2018-01-09 23:17:29 +0100

    Fix entry fee in details

--- inc/concert-dates.php
+++ inc/concert-dates.php
@@ -1,6 +1,6 @@
 <ul><?php foreach($concerts as $concert) {
     $entry = '';
-    if(!empty($concert['fee']) && $concert['fee'] != '-1') {
+    if($concert['fee'] != '-1') {
         $entry = ', Eintritt: '.(empty($concert['fee']) ? 'frei, Kollekte' : $concert['fee'].' CHF');
     } ?>
     <li><time datetime="<?php echo $concert['date'] ?>"><?php echo get_the_date('l j. F Y, H:i', $concert['id']) ?></time> Uhr, <?php echo $concert['location'].$entry ?></li>
