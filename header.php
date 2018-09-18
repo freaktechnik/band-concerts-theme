5901e993de79a171557279e8f1503c4beac33a1c
Martin Giger <martin@humanoids.be>
2018-09-18 20:24:09 +0200

    feat: display notch support. Or something along those lines at least

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php esc_attr(bloginfo('charset')); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <link rel="profile" href="http://gmpg.org/xfn/11">
 
