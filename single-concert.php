7c8889856c91b25bd7c4cb7ba5a63dffbfc9c8ca
Martin Giger <martin@humanoids.be>
2021-09-03 13:09:43 +0200

    fix: single event ical export date and details

--- single-concert.php
+++ single-concert.php
@@ -1,8 +1,9 @@
 <?php
 while(have_posts()) {
     the_post();
+    $concert = get_post();
     $post = \BandConcerts\ConcertSeries::getSeriesForConcert(get_the_ID());
-    $concerts = \BandConcerts\Concert::FormatPost(get_post(), $post->ID);
+    $concerts = \BandConcerts\Concert::FormatPost($concert, $post->ID);
     $cal = \BandConcerts\EventICal::MakeSingleEvent($concerts);
     $cal->emit($post->post_title.'.ics');
     return;
